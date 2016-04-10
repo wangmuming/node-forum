24b2539d270a534c3831119dbda08282503d906f
wangmuming <wangmuming_0218@126.com>
2016-04-11 01:20:40 +0800

    commit by wangmuming

---
+++ src/middlewares/user.js
@@ -0,0 +1,39 @@
+'use strict';
+
+
+/**
+* practice Node.js project
+*
+* @author William Wang <wangmuming_0218@126.com>
+*/
+
+module.exports = function (done) {
+
+
+  $.checkLogin = function (req, res, next) {
+
+    if (!(req.session.user && req.session.user._id)) return next(new Error('please login firstly'));
+
+    next();
+
+  };
+
+
+  $.checkTopicAuthor = async function (req, res, next) {
+
+    const topic = await $.method('topic.get').call({_id: req.params.topic_id});
+    if (!topic) return next(new Error(`topic ${req.params.topic_id} does not exists`));
+
+    if (topic.authorId.toString() !== req.session.user._id.toString()) {
+      return next(new Error('access denied'));
+    }
+
+    req.topic = topic;
+    next();
+
+  };
+
+
+  done();
+
+};
