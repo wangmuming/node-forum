77abfdf2f7fec5f02fd37e8e269532ea1d2edd94
wangmuming <wangmuming_0218@126.com>
2016-04-11 00:56:43 +0800

    'update'

--- src/init/express.js
+++ src/init/express.js
@@ -10,24 +10,60 @@ import path from 'path';
 import express from 'express';
 import serveStatic from 'serve-static';
 import bodyParser from 'body-parser';
-import multiparty from 'multiparty';
+import multipart from 'connect-multiparty';
+import session from 'express-session';
+import _RedisStore from 'connect-redis';
+const RedisStore = _RedisStore(session);
+
 
 module.exports = function (done) {
-  
-   const app = express();
 
-   app.use(bodyParser.json());
-   app.use(bodyParser.urlencoded({extended:false}));
+  const debug = $.createDebug('init:express');
+  debug('initing Express...');
 
-   const router = express.Router();
-   $.router = router;
+  const app = express();
 
-   app.use(router);
-   app.use('/static',serveStatic(path.resolve(__dirname,'../../static')));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(multipart());
+  app.use(session({
+    secret: $.config.get('web.session.secret'),
+    store: new RedisStore($.config.get('web.session.redis')),
+  }));
 
-   app.listen($.config.get('web.port'), (err) => {
-     done(err);
-   });
-  
+  const router = express.Router();
+
+  const routerWrap = {};
+  ['get', 'head', 'post', 'put', 'del', 'delete'].forEach(method => {
+    routerWrap[method] = function (path, ...fnList) {
+      fnList = fnList.map(fn => {
+        return function (req, res, next) {
+          const ret = fn(req, res, next);
+          if (ret && ret.catch) ret.catch(next);
+        };
+      });
+      router[method](path, ...fnList);
+    };
+  });
+  $.router = routerWrap;
+
+  app.use(function (req, res, next) {
+    res.apiSuccess = function (data) {
+      res.json({success: true, result: data});
+    };
+    next();
+  });
+
+  app.use(router);
+  app.use('/static', serveStatic(path.resolve(__dirname, '../../static')));
+
+  app.use('/api', function (err, req, res, next) {
+    debug('API error: %s', err && err.stack || err);
+    res.json({error: err.toString()});
+  });
+
+  app.listen($.config.get('web.port'), (err) => {
+    done(err);
+  });
 
 };
--- src/init/mongodb.js
+++ src/init/mongodb.js
@@ -11,11 +11,16 @@ import mongoose from 'mongoose';
 
 module.exports = function (done) {
 
-   const conn = mongoose.createConnection($.config.get('db.mongodb'));	
-   
-   $.mongodb = conn;
-   
-   $.model = {};
+  const debug = $.createDebug('init:mongodb');
+  debug('connecting to MongoDB...');
 
-   done();
+  const conn = mongoose.createConnection($.config.get('db.mongodb'));
+  $.mongodb = conn;
+  $.model = {};
+
+  const ObjectId = mongoose.Types.ObjectId;
+  $.utils.ObjectId = ObjectId;
+
+  done();
+
 }
