be4217f0d7dd0c22be228b2f00e63f57d382152b
wangmuming <wangmuming_0218@126.com>
2016-05-01 22:35:56 +0800

    update file

--- frontend/entry.js
+++ frontend/entry.js
@@ -1,7 +1,30 @@
-import 'bootstrap-webpack';
+window.jQuery = require('jquery');
+require('bootstrap-webpack');
+
+// import 'bootstrap-webpack';
+import { Router, Route, Link, browserHistory } from 'react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import TopicDetail from './component/TopicDetail';
+import Login from './component/Login';
+import NewTopic from './component/NewTopic';
+import EditTopic from './component/EditTopic';
 
-ReactDOM.render(<App />, document.body);
+
+// console.log('hello. world');
+const e = document.createElement('div');
+e.id = 'app';
+document.body.appendChild(e);
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="/topic/:id" component={TopicDetail}/>
+      <Route path="/topic/:id/edit" component={EditTopic}/>
+      <Route path="new" component={NewTopic}/>
+      <Route path="/login" component={Login}/>
+    </Route>
+  </Router>
+), e);
